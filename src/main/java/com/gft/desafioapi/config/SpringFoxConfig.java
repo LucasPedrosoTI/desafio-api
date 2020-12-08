f9f60725bb46ccaee55e7cccdd1edd2ce0612291
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-12-08 16:45:32 -0300

    impl get token diretamente no swagger ui

--- src/main/java/com/gft/desafioapi/config/SpringFoxConfig.java
+++ src/main/java/com/gft/desafioapi/config/SpringFoxConfig.java
@@ -1,53 +1,83 @@
 package com.gft.desafioapi.config;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.OAuth;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig implements WebMvcConfigurer {
 
+	private String accessTokenUri = "http://localhost:8080/oauth/token";
+
 	@Bean
 	public Docket api() {
 
-		List<ResponseMessage> list = new java.util.ArrayList<>();
-		list.add(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Result"))
-				.build());
-		list.add(new ResponseMessageBuilder().code(401).message("Unauthorized").responseModel(new ModelRef("Result"))
-				.build());
-		list.add(new ResponseMessageBuilder().code(406).message("Not Acceptable").responseModel(new ModelRef("Result"))
-				.build());
-
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.gft.desafioapi")).paths(PathSelectors.any()).build()
-				.globalOperationParameters(commonParameters()).apiInfo(apiInfo())
+				.apiInfo(apiInfo())
 				.tags(new Tag("Fornecedores", "Gerencia os fornecedores"), new Tag("Clientes", "Gerencia os clientes"),
-						new Tag("Produtos", "Gerencia os produtos"), new Tag("Vendas", "Gerencia as vendas"));
-		// .securitySchemes(Collections.singletonList(securitySchema()))
-		// .securityContexts(Collections.singletonList(securityContext())).pathMapping("/")
-		// .useDefaultResponseMessages(false)
-		// .globalResponseMessage(RequestMethod.GET,
-		// list).globalResponseMessage(RequestMethod.POST, list);
+						new Tag("Produtos", "Gerencia os produtos"), new Tag("Vendas", "Gerencia as vendas"))
+				.securityContexts(Collections.singletonList(securityContext()))
+				.securitySchemes(Arrays.asList(securitySchema()));
+		// .globalOperationParameters(commonParameters())
 
+	}
+
+	private OAuth securitySchema() {
+
+		List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+		authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+
+		List<GrantType> grantTypes = new ArrayList<>();
+		GrantType passwordCredentialsGrant = new ResourceOwnerPasswordCredentialsGrant(accessTokenUri);
+
+		grantTypes.add(passwordCredentialsGrant);
+
+		return new OAuth("oauth2", authorizationScopeList, grantTypes);
+
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.ant("/api/**"))
+				.build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+
+		final AuthorizationScope[] authorizationScopes = { new AuthorizationScope("read", "read all") };
+
+		return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
+	}
+
+	@Bean
+	public SecurityConfiguration securityInfo() {
+		return new SecurityConfiguration("admin", "admin", "", "", "Bearer access token", ApiKeyVehicle.HEADER,
+				HttpHeaders.AUTHORIZATION, "");
 	}
 
 	public ApiInfo apiInfo() {
@@ -56,48 +86,16 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.contact(new Contact("GFT", "https://www.gft.com", "lucas.pedroso@gft.com")).build();
 	}
 
-	private List<Parameter> commonParameters() {
-		List<Parameter> parameters = new ArrayList<Parameter>();
-		parameters.add(new ParameterBuilder().name("access_token").description("Token for authorization")
-				.modelRef(new ModelRef("string")).parameterType("query").required(true).build());
+	// MANUALLY ADD INPUT FOR ACCESS TOKEN TO ALL ENDPOINTS
 
-		return parameters;
-	}
-
-//	private OAuth securitySchema() {
-//
-//		List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
-//		authorizationScopeList.add(new AuthorizationScope("read", "read all"));
-//		authorizationScopeList.add(new AuthorizationScope("trust", "trust all"));
-//		authorizationScopeList.add(new AuthorizationScope("write", "access all"));
-//
-//		List<GrantType> grantTypes = new ArrayList<>();
-//		GrantType creGrant = new ResourceOwnerPasswordCredentialsGrant("http://localhost:8080/oauth/token");
-//
-//		grantTypes.add(creGrant);
-//
-//		return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
-//
-//	}
-//
-//	private SecurityContext securityContext() {
-//		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.ant("/api/**"))
-//				.build();
-//	}
-//
-//	private List<SecurityReference> defaultAuth() {
-//
-//		final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
-//		authorizationScopes[0] = new AuthorizationScope("read", "read all");
-//		authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
-//		authorizationScopes[2] = new AuthorizationScope("write", "write all");
-//
-//		return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
-//	}
-//
-//	@Bean
-//	public SecurityConfiguration securityInfo() {
-//		return new SecurityConfiguration("admin", "admin", "", "", "", ApiKeyVehicle.HEADER, "", " ");
-//	}
+	// private List<Parameter> commonParameters() {
+	// List<Parameter> parameters = new ArrayList<Parameter>();
+	// parameters.add(new ParameterBuilder().name("access_token").description("Token
+	// for authorization")
+	// .modelRef(new
+	// ModelRef("string")).parameterType("query").required(true).build());
+	//
+	// return parameters;
+	// }
 
 }
