2294456cdc86157d77d46fbda3d1104f1db2d8ca
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-12-18 21:41:32 -0300

    tests fornecedor service

---
+++ src/test/java/com/gft/desafioapi/service/FornecedorServiceTest.java
@@ -0,0 +1,69 @@
+package com.gft.desafioapi.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.gft.desafioapi.model.Fornecedor;
+import com.gft.desafioapi.repository.FornecedorRepository;
+import com.gft.desafioapi.utils.Constants;
+
+public class FornecedorServiceTest {
+
+	@Mock
+	private FornecedorRepository fornecedorRepository;
+
+	@InjectMocks
+	private FornecedorService fornecedorService;
+
+	private Fornecedor fornecedor;
+
+	@BeforeEach
+	void setup() throws Exception {
+		fornecedor = new Fornecedor(1L, "Nome", "12345678912345", null);
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void deveCriarFornecedorSetandoIdNull() throws Exception {
+
+		Fornecedor fornecedorFinal = new Fornecedor(null, "Nome", "12345678912345", null);
+
+		fornecedorService.create(fornecedor);
+
+		verify(fornecedorRepository).save(fornecedorFinal);
+	}
+
+	@Test
+	void deveAtualizaFornecedor() throws Exception {
+
+		Fornecedor atualizacao = new Fornecedor(null, "Apple", null, null);
+
+		when(fornecedorRepository.findById(1L)).thenReturn(Optional.of(fornecedor));
+
+		fornecedor.setNome("Apple");
+
+		fornecedorService.update(1L, atualizacao);
+
+		verify(fornecedorRepository).save(fornecedor);
+	}
+
+	@Test
+	void deveRetornarMapSuccessTrue() throws Exception {
+
+		doNothing().when(fornecedorRepository).deleteById(1L);
+
+		assertEquals(Constants.MAP_SUCCESS_TRUE, fornecedorService.delete(1L));
+
+	}
+
+}
